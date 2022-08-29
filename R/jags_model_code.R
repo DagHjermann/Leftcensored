4ae76750fe059315f25f6f1243a372c47251055e
Dag_Hjermann <dhj@niva.no>
2022-08-29 16:16:29 +0200

    Add script for jags code for thin plate splines

---
+++ R/jags_model_code.R
@@ -0,0 +1,69 @@
+model_tp_k3_leftcensored = '
+model {
+  mu <- X %*% b ## expected response
+  for (i in 1:n) { y[i] ~ dnorm(mu[i], tau) } ## response 
+  for (j in 1:m) {
+    Z[j] ~ dbern(prob[j])
+    prob[j] <- max(pnorm(cut[j], mu[n+j], tau), 0.01)
+  }  
+  scale <- 1/tau ## convert tau to standard GLM scale
+  tau ~ dgamma(.05,.005) ## precision parameter prior 
+  ## Parametric effect priors CHECK tau=1/79^2 is appropriate!
+  for (i in 1:1) { b[i] ~ dnorm(0,0.00016) }
+  ## prior for s(x2)... 
+  K1 <- S1[1:2,1:2] * lambda[1]  + S1[1:2,3:4] * lambda[2]
+  b[2:3] ~ dmnorm(zero[2:3],K1) 
+  ## smoothing parameter priors CHECK...
+  for (i in 1:2) {
+    lambda[i] ~ dgamma(.05,.005)
+    rho[i] <- log(lambda[i])
+  }
+}
+'
+
+model_tp_k4_leftcensored = '
+model {
+  mu <- X %*% b ## expected response
+  for (i in 1:n) { y[i] ~ dnorm(mu[i], tau) } ## response 
+  for (j in 1:m) {
+    Z[j] ~ dbern(prob[j])
+    prob[j] <- max(pnorm(cut[j], mu[n+j], tau), 0.01)
+  }  
+  scale <- 1/tau ## convert tau to standard GLM scale
+  tau ~ dgamma(.05,.005) ## precision parameter prior 
+  ## Parametric effect priors CHECK tau=1/79^2 is appropriate!
+  for (i in 1:1) { b[i] ~ dnorm(0,0.00016) }
+  ## prior for s(x2)... 
+  for (i in c(2:3)) { b[i] ~ dnorm(0, lambda[1]) }
+  for (i in c(4)) { b[i] ~ dnorm(0, lambda[2]) }
+  ## smoothing parameter priors CHECK...
+  for (i in 1:2) {
+    lambda[i] ~ dgamma(.05,.005)
+    rho[i] <- log(lambda[i])
+  }
+}
+'
+
+model_tp_k5_leftcensored = '
+model {
+  mu <- X %*% b ## expected response
+  for (i in 1:n) { y[i] ~ dnorm(mu[i], tau) } ## response 
+  for (j in 1:m) {
+    Z[j] ~ dbern(prob[j])
+    prob[j] <- max(pnorm(cut[j], mu[n+j], tau), 0.01)
+  }  
+  scale <- 1/tau ## convert tau to standard GLM scale
+  tau ~ dgamma(.05,.005) ## precision parameter prior 
+  ## Parametric effect priors CHECK tau=1/79^2 is appropriate!
+  for (i in 1:1) { b[i] ~ dnorm(0,0.00016) }
+  ## prior for s(x2)... 
+  for (i in c(2:4)) { b[i] ~ dnorm(0, lambda[1]) }
+  for (i in c(5)) { b[i] ~ dnorm(0, lambda[2]) }
+  ## smoothing parameter priors CHECK...
+  for (i in 1:2) {
+    lambda[i] ~ dgamma(.05,.005)
+    rho[i] <- log(lambda[i])
+  }
+}
+'
+
